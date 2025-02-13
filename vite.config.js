087d405f7756e5c1f0ec9e7df7b1f2890c1ba31c
kiku-hub <tacyskani@gmail.com>
2025-02-13 16:41:31 +0900

    デプロイ設定

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ORCX_WEBSITE_NEW/',
   build: {
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
       }
     }
   }
