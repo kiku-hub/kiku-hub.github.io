4c0d3ab1e73d669b8ad274965395eb3f7ff8b246
kiku-hub <tacyskani@gmail.com>
2025-02-13 16:47:08 +0900

    デプロイ修正

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ORCX_WEBSITE_NEW/',
+  base: '/',
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
